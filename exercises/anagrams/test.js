d1340f1bb668787d901d01c2b2840fac2c1d2b83
saad <saad@lizard-apps.com>
2018-10-29 11:53:44 +0800

    anagrams done

--- exercises/anagrams/test.js
+++ exercises/anagrams/test.js
@@ -1,4 +1,5 @@
 const anagrams = require('./index.js');
+const anagram1 = require('./index.js');
 
 test('anagrams function exists', () => {
   expect(typeof anagrams).toEqual('function');
@@ -23,5 +24,33 @@ test('"One one" is not an anagram of "One one c"', () => {
 test('"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"', () => {
   expect(
     anagrams('A tree, a life, a bench', 'A tree, a fence, a yard')
+  ).toBeFalsy();
+});
+/**------------- */
+
+
+test('anagrams function exists', () => {
+  expect(typeof anagram1).toEqual('function');
+});
+
+test('"hello" is an anagram of "llohe"', () => {
+  expect(anagram1('hello', 'llohe')).toBeTruthy();
+});
+
+test('"Whoa! Hi!" is an anagram of "Hi! Whoa!"', () => {
+  expect(anagram1('Whoa! Hi!', 'Hi! Whoa!')).toBeTruthy();
+});
+
+test('"One One" is not an anagram of "Two two two"', () => {
+  expect(anagram1('One One', 'Two two two')).toBeFalsy();
+});
+
+test('"One one" is not an anagram of "One one c"', () => {
+  expect(anagram1('One one', 'One one c')).toBeFalsy();
+});
+
+test('"A tree, a life, a bench" is not an anagram of "A tree, a fence, a yard"', () => {
+  expect(
+    anagram1('A tree, a life, a bench', 'A tree, a fence, a yard')
   ).toBeFalsy();
 });
